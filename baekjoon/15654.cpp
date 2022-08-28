80d7427a5b959692e7f0e9a7dd08977870c35abb
junyoung-sim <jysim.ny@gmail.com>
2022-08-28 16:32:20 -0400

    오랜만에백준

---
+++ baekjoon/15654.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int N, M;
+std::vector<int> num;
+
+void func(std::string seq, int N, int M) {
+    if(seq.length() == M) {
+        for(int i = 0; i < M; i++) {
+            std::cout << num[seq[i] - '0'];
+            if(i != M - 1) std::cout << " ";
+        }
+        std::cout << "\n";
+        return;
+    }
+
+    for(int n = 0; n < N; n++) {
+        bool viable = true;
+        for(char ch: seq) {
+            viable = ch - '0' != n;
+            if(!viable) break;
+        }
+
+        if(viable) func(seq + std::to_string(n), N, M);
+    }
+}
+
+int main()
+{
+    std::cin >> N >> M;
+    num.resize(N);
+    for(int i = 0; i < N; i++) std::cin >> num[i];
+
+    for(int i = 0; i < N-1; i++) {
+        int min_pos = i;
+        for(int j = i; j < N; j++) {
+            if(num[j] < num[min_pos])
+                min_pos = j;
+        }
+        std::swap(num[i], num[min_pos]);
+    }
+
+    for(int n = 0; n < N; n++)
+        func(std::to_string(n), N, M);
+
+    return 0;
+}
