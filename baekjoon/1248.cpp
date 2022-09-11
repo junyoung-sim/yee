0bab30b045cafbbc88bf7e1fc949781ef05322c0
junyoung-sim <jysim.ny@gmail.com>
2022-09-10 21:20:18 -0400

    골드3문제

--- baekjoon/1248.cpp
+++ baekjoon/1248.cpp
@@ -2,23 +2,70 @@
 #include <string>
 #include <vector>
 
-int n;
-std::string code = "";
-std::vector<std::vector<char>> matrix;
+int N;
+std::string sign;
+std::vector<std::vector<int>> sign_matrix;
+std::vector<int> code;
 
-std::string seq = "";
+bool func(int n, int i) {
+    code[n] = i;
+
+    bool valid = false;
+    for(int j = 0; j <= n; j++) {
+        int sum = 0;
+        for(int k = j; k <= n; k++) {
+            sum += code[k];
+            if(sign_matrix[j][k] == '-') valid = (sum < 0);
+            else if(sign_matrix[j][k] == '+') valid = (sum > 0);
+            else valid = (sum == 0);
+
+            if(!valid) break;
+        }
+
+        if(!valid) break;
+    }
+
+    if(valid && n == N-1)
+        return true;
+    else if(valid && n != N-1) {
+        for(int k = -10; k <= 10; k++) {
+            valid = func(n+1, k);
+            if(valid) break;
+        }
+
+        return valid;
+    }
+    else
+        return false;
+}
 
 int main()
 {
-    std::cin >> n;
-    std::cin >> code;
+    std::cin >> N;
+    std::cin >> sign;
 
-    matrix.resize(n, std::vector<char>(n));
-    int c = 0;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if(j >= i) matrix[i][j] = code[c++];
+    int k = 0;
+    sign_matrix.resize(N, std::vector<int>(N));
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            if(j >= i)
+                sign_matrix[i][j] = sign[k++];
+            else
+                sign_matrix[i][j] = -1;
         }
+    }
+
+    code.resize(N);
+
+    for(int i = -10; i <= 10; i++) {
+        bool valid = func(0, i);
+        if(valid) break;
+    }
+
+    for(int i = 0; i < code.size(); i++) {
+        std::cout << code[i];
+        if(i != code.size() - 1) std::cout << " ";
+        else std::cout << "\n";
     }
 
     return 0;
