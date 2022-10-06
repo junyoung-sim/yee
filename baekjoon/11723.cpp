b529dec0b3a3009b1b60ecfdcac2e0bcc7bcd660
junyoung-sim <jysim.ny@gmail.com>
2022-10-06 19:01:20 -0400

    백업

---
+++ baekjoon/11723.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> group(21, 0);
+
+int M;
+std::vector<std::string> cmd;
+std::vector<int> val;
+
+std::string ans = "";
+
+int main()
+{
+    std::cin >> M;
+    
+
+    return 0;
+}
