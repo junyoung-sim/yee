630676d322109560583e9a5d3d4827a0bdb16f23
junyoung-sim <eden920.dev@gmail.com>
2022-10-09 22:26:01 -0400

    2022-10-09

--- baekjoon/14391.cpp
+++ baekjoon/14391.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 std::vector<int> mask;
+std::vector<std::vector<int>> cut;
 std::vector<std::vector<int>> map;
 
 int N, M;
@@ -10,6 +12,23 @@ int ans = 0;
 void func(int i, int n) {
     mask[i] = n;
     if(i == mask.size() -1) {
+        int m = 0;
+        for(int r = 0; r < N; r++)
+            for(int c = 0; c < M; c++)
+                cut[r][c] = mask[m++];
+
+        std::string num = "";
+        int sum = 0;
+
+        for(int c = 0; c < M; c++) {
+            
+        }
+
+        for(int r = 0; r < N; r++) {
+            
+        }
+
+        ans = std::max(sum, ans);
 
         return;
     }
@@ -23,14 +42,20 @@ int main()
     std::cin >> N >> M;
 
     mask.resize(N*M);
+    cut.resize(N, std::vector<int>(M));
     map.resize(N, std::vector<int>(M));
 
-    for(int r = 0; r < N; r++)
+    std::string row;
+    for(int r = 0; r < N; r++) {
+        std::cin >> row;
         for(int c = 0; c < M; c++)
-            std::cin >> map[r][c];
+            map[r][c] = (row[c] - '0');
+    }
 
     func(0, 0);
     func(0, 1);
+
+    std::cout << ans << "\n";
 
     return 0;
 }
