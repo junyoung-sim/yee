86ef621c53f9abc83737f9e440b06ab67d190768
junyoung-sim <jysim.ny@gmail.com>
2023-02-26 14:24:09 -0500

    2023-02-26

---
+++ leet/121.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        for(int i = 1; i < prices.size(); i++) {
+            int diff = prices[i] - prices[i-1];
+            if(diff > 0) {
+                ans = max(diff, ans);
+                prices[i] = prices[i-1];
+            }
+        }
+        return ans;
+    }
+};
