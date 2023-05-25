2932c32b046b7b0827621b8754dfccc6bb063634
junyoung-sim <jysim.ny@gmail.com>
2023-05-25 15:43:27 -0400

    2023-05-25

--- baekjoon/2108.cpp
+++ baekjoon/2108.cpp
@@ -21,32 +21,36 @@ int main()
     std::vector<int> v(N);
     std::vector<int> freq(8001, 0); // -4000 ~ 4000
 
-    double sum = 0;
     for(int i = 0; i < N; i++) {
         std::cin >> v[i];
-        sum += v[i];   
         freq[v[i] + 4000]++;
     }
-    std::sort(v.begin(), v.end());
 
-    int t; int max_freq = 0;
-    for(int i = 0; i <= 8000; i++) {
-        if(freq[i] > max_freq) {
-            t = i;
-            max_freq = freq[i];
-        }
-    }
-    for(int i = t + 1; i <= 8000; i++) {
-        if(freq[i] == max_freq) {
-            t = i;
+    double mean = 0;
+    for(int &x: v)
+        mean += x;
+    mean /= N;
+    mean += (mean < 0 ? -0.5 : 0.5);
+    std::cout << (int)mean << "\n";
+
+    std::sort(v.begin(), v.end());
+    std::cout << v[N/2] << "\n";
+
+    v.erase(std::unique(v.begin(), v.end()), v.end());
+    int max_freq = *std::max_element(freq.begin(), freq.end());
+
+    int mode = -RAND_MAX;
+    for(int &x: v) {
+        if(mode == -RAND_MAX && freq[x+4000] == max_freq)
+            mode = x;
+        else if(mode != -RAND_MAX && freq[x+4000] == max_freq) {
+            mode = x;
             break;
         }
     }
+    std::cout << mode << "\n";
 
-    std::cout << std::round(sum / N) << "\n"; // mean
-    std::cout << v[(N-1)/2] << "\n"; // median
-    std::cout << t - 4000 << "\n";
-    std::cout << v[N-1] - v[0] << "\n"; // range
+    std::cout << v[v.size() - 1] - v[0] << "\n";
 
     return 0;
 }
