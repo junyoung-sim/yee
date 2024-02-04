861554fbfad81eda09f460bf6a9c36531f3c7ee5
junyoung-sim <jysim.ny@gmail.com>
2024-02-03 23:27:44 -0500

    2024-02-03

--- baekjoon/2667.cpp
+++ baekjoon/2667.cpp
@@ -13,22 +13,59 @@
 #include <queue>
 #include <deque>
 
+int N;
+const int dr[4] = {-1, 0, 1, 0};
+const int dc[4] = {0, 1, 0, -1};
+
+std::vector<std::vector<int>> graph;
+std::vector<std::vector<int>> visit;
+
+int count = 0;
+void dfs(int i, int j) {
+	visit[i][j] = 1; count++;
+	for(int n = 0; n < 4; n++) {
+		if(i+dr[n] >= 0 && i+dr[n] < N &&
+		   j+dc[n] >= 0 && j+dc[n] < N &&
+		   !visit[i+dr[n]][j+dc[n]]) dfs(i+dr[n], j+dc[n]);
+	}
+}
+
 int main()
 {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	std::cout.tie(NULL);
 
-	int N;
 	std::cin >> N;
+	graph.resize(N, std::vector<int>(N));
+	visit.resize(N, std::vector<int>(N));
 
 	std::string str;
-	std::vector<std::vector<int>> graph(N, std::vector<int>(N,0));
-	std::vector<std::vector<int>> visit(N, std::vector<int>(N,0));
 	for(int i = 0; i < N; i++) {
 		std::cin >> str;
-
+		for(int j = 0; j < N; j++) {
+			graph[i][j] = str[j] - '0';
+			visit[i][j] = !graph[i][j];
+		}
 	}
-	
+
+	std::vector<int> cmp;
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			if(!visit[i][j]) {
+				dfs(i, j);
+				cmp.push_back(count);
+				count = 0;
+			}
+		}
+	}
+
+	std::cout << cmp.size() << "\n";
+	if(cmp.size()) {
+		std::sort(cmp.begin(), cmp.end());
+		for(int &x: cmp)
+			std::cout << x << "\n";
+	}
+
 	return 0;
 }
