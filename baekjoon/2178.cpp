070f3273b235be3f51214bc51eeaccd6a8bc550a
junyoung-sim <jysim.ny@gmail.com>
2024-02-05 15:26:15 -0500

    2024-02-05

---
+++ baekjoon/2178.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <deque>
+
+struct pos {
+	int r, c;
+	pos(int i, int j) { r = i; c = j; }
+};
+
+int N, M;
+std::vector<std::vector<int>> graph;
+
+int dr[4] = {-1,0,1,0};
+int dc[4] = {0,1,0,-1};
+
+int bfs(int i, int j) {
+	std::queue<pos> q;
+	q.push(pos(i, j));
+
+	while(!q.empty()) {
+		pos x = q.front(); q.pop();
+		int r = x.r, c = x.c;
+		for(int n = 0; n < 4; n++) {
+			if(r+dr[n] >= 0 && r+dr[n] < N && c+dc[n] >= 0 && c+dc[n] < M && graph[r+dr[n]][c+dc[n]] == 1) {
+				graph[r+dr[n]][c+dc[n]] = graph[r][c] + 1;
+				q.push(pos(r+dr[n], c+dc[n]));
+			}
+		}
+	}
+
+	return graph[N-1][M-1];
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	std::cin >> N >> M;
+	graph.resize(N, std::vector<int>(M));
+
+	std::string str;
+	for(int i = 0; i < N; i++) {
+		std::cin >> str;
+		for(int j = 0; j < M; j++)
+			graph[i][j] = str[j] - '0';
+	}
+
+	std::cout << bfs(0, 0) << "\n";
+
+	return 0;
+}
